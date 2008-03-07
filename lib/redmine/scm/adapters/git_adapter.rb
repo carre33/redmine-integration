d857aaeea485d6308a9bbbc8575237c1b331e732
John Goerzen <jgoerzen@complete.org>
2008-03-06 18:34:04 -0600

    M-x indent-region on git_adapter.rb

--- lib/redmine/scm/adapters/git_adapter.rb
+++ lib/redmine/scm/adapters/git_adapter.rb
@@ -21,65 +21,65 @@ module Redmine
   module Scm
     module Adapters    
       class GitAdapter < AbstractAdapter
-      
+        
         # Git executable name
         GIT_BIN = "git"
         
 
         #get the revision of a particuliar file
 	def get_rev (rev,path)
-cmd="cd #{target('')} && git show #{rev} #{path}" if rev!='latest'
-cmd="cd #{target('')} && git log -1 master -- #{path}" if 
-rev=='latest' or rev.nil?
-rev=[]
- i=0
- puts "get_rev"
- puts cmd
-    shellout(cmd) do |io|
-          commit_files=[]
-          params={:commit=>'',:author=>'',:date=>'',:message=>'',:file=>{:path=>'',:action=>''}}
-         
-          message=''
-	io.each_line do |line|
+          cmd="cd #{target('')} && git show #{rev} #{path}" if rev!='latest'
+          cmd="cd #{target('')} && git log -1 master -- #{path}" if 
+            rev=='latest' or rev.nil?
+          rev=[]
+          i=0
+          puts "get_rev"
+          puts cmd
+          shellout(cmd) do |io|
+            commit_files=[]
+            params={:commit=>'',:author=>'',:date=>'',:message=>'',:file=>{:path=>'',:action=>''}}
+            
+            message=''
+            io.each_line do |line|
 
-	        i=0 if line=~/^commit/
-		params[:commit]=line.chomp.gsub("commit ",'') if i==0
-		
-		params[:author]=line.chomp.gsub("Author: ",'') if i==1
-		params[:date]=line.chomp.gsub("Date: ",'') if i==2
-		params[:message]+= line.chomp.to_s if i==4 and line[0..0]!=':'
-		params[:file][:action], params[:file][:path]= line.chomp.slice(/[ACDMRTUXB].*/).split(' ', 2) if i>=4 and line[0..0]==':'
-		commit_files << {:action=>params[:file][:action],:path=>params[:file][:path]}  if i>=4 and line[0..0]==':'
-		i+=1
-		end	
-		
-		rev = Revision.new({:identifier => nil,
-                                       :scmid => params[:commit],
-                                       :author => params[:author],
-                                       :time => Time.parse(params[:date]),
-                                       :message => params[:message],
-                                       :paths => commit_files
-            				})
-	end
+              i=0 if line=~/^commit/
+              params[:commit]=line.chomp.gsub("commit ",'') if i==0
+              
+              params[:author]=line.chomp.gsub("Author: ",'') if i==1
+              params[:date]=line.chomp.gsub("Date: ",'') if i==2
+              params[:message]+= line.chomp.to_s if i==4 and line[0..0]!=':'
+              params[:file][:action], params[:file][:path]= line.chomp.slice(/[ACDMRTUXB].*/).split(' ', 2) if i>=4 and line[0..0]==':'
+              commit_files << {:action=>params[:file][:action],:path=>params[:file][:path]}  if i>=4 and line[0..0]==':'
+              i+=1
+            end	
+            
+            rev = Revision.new({:identifier => nil,
+                                 :scmid => params[:commit],
+                                 :author => params[:author],
+                                 :time => Time.parse(params[:date]),
+                                 :message => params[:message],
+                                 :paths => commit_files
+                               })
+          end
 
-	get_rev('latest',path) if i==0
+          get_rev('latest',path) if i==0
 
           return nil if $? && $?.exitstatus != 0
           return rev
-#         rescue Errno::ENOENT => e
-#           raise CommandFailed
-end
+          #         rescue Errno::ENOENT => e
+          #           raise CommandFailed
+        end
 
 
         def info
-#           cmd = "#{GIT_BIN} -R #{target('')} root"
-#           root_url = nil
-#           shellout(cmd) do |io|
-             root_url = target('')
-#           end
+          #           cmd = "#{GIT_BIN} -R #{target('')} root"
+          #           root_url = nil
+          #           shellout(cmd) do |io|
+          root_url = target('')
+          #           end
           info = Info.new({:root_url => target(''),
-                           :lastrev => revisions(root_url,nil,nil,nil).first
-                         })
+                            :lastrev => revisions(root_url,nil,nil,nil).first
+                          })
           info
         rescue Errno::ENOENT => e
           return nil
@@ -90,25 +90,25 @@ end
           entries = Entries.new
           cmd = "cd #{target('')} && #{GIT_BIN} show HEAD:#{path}" if identifier.nil?
           cmd = "cd #{target('')} && #{GIT_BIN} show #{identifier}:#{path}" if identifier
-	shellout(cmd)  do |io|
-	 io.each_line do |line|
+          shellout(cmd)  do |io|
+            io.each_line do |line|
               e = line.chomp.split('\\')
-	unless e.to_s.strip=='' or line[0..3]=='tree'
-	name=e.first.split('/')[0]
-              entries << Entry.new({:name => name,
-                                    :path => (path.empty? ? name : "#{path}/#{name}"),
-                                    :kind => ((e.first.include? '/') ? 'dir' : 'file'),
-                                    :lastrev => get_rev(identifier,(path.empty? ? name : "#{path}/#{name}"))
-                                    }) unless entries.detect{|entry| entry.name == name}
-         end
-	end
-	end
+              unless e.to_s.strip=='' or line[0..3]=='tree'
+                name=e.first.split('/')[0]
+                entries << Entry.new({:name => name,
+                                       :path => (path.empty? ? name : "#{path}/#{name}"),
+                                       :kind => ((e.first.include? '/') ? 'dir' : 'file'),
+                                       :lastrev => get_rev(identifier,(path.empty? ? name : "#{path}/#{name}"))
+                                     }) unless entries.detect{|entry| entry.name == name}
+              end
+            end
+          end
           return nil if $? && $?.exitstatus != 0
           entries.sort_by_name
-#         rescue Errno::ENOENT => e
-#           raise CommandFailed
+          #         rescue Errno::ENOENT => e
+          #           raise CommandFailed
         end
-  
+        
         def entry(path=nil, identifier=nil)
           path ||= ''
           search_path = path.split('/')[0..-2].join('/')
@@ -116,51 +116,51 @@ end
           e = entries(search_path, identifier)
           e ? e.detect{|entry| entry.name == entry_name} : nil
         end
-          
-		def revisions(path, identifier_from, identifier_to, options={})
-		revisions = Revisions.new
-		cmd = "cd #{target('')} && #{GIT_BIN} whatchanged "
-		cmd << " #{identifier_from}.. " if identifier_from
-		cmd << " #{identifier_to} " if identifier_to
-                #cmd << " HEAD " if !identifier_to
-                puts "revisions"
-                puts cmd
-		shellout(cmd) do |io|
-		files=[]
-		params={:commit=>'',:author=>'',:date=>'',:message=>'',:file=>{:path=>'',:action=>''}}
-		i=0
-		message=''
-		io.each_line do |line|
-	
-			if line=~/^commit/ and i>0
-			revisions << Revision.new({:identifier => nil,
-					:scmid => params[:commit],
-					:author => params[:author],
-					:time => Time.parse(params[:date]),
-					:message => params[:message],
-					:paths => files
-						})
-	
-			files=[]	
-			i=0
-			params={:commit=>'',:author=>'',:date=>'',:message=>'',:file=>{:path=>'',:action=>''}}
-			end
-			params[:commit]=line.chomp.gsub("commit ",'') if i==0
-			params[:author]=line.chomp.gsub("Author: ",'') if i==1
-			params[:date]=line.chomp.gsub("Date: ",'') if i==2
-			params[:message]+= line.chomp.to_s if i>=4 and line[0..0]!=':'
-			params[:file][:action], params[:file][:path]= line.chomp.slice(/[ACDMRTUXB].*/).split(' ', 2) if i>=4 and line[0..0]==':'
-			files << {:action=>params[:file][:action],:path=>params[:file][:path]}  if i>=4 and line[0..0]==':'
-			i+=1
-			end	
-		end
+        
+        def revisions(path, identifier_from, identifier_to, options={})
+          revisions = Revisions.new
+          cmd = "cd #{target('')} && #{GIT_BIN} whatchanged "
+          cmd << " #{identifier_from}.. " if identifier_from
+          cmd << " #{identifier_to} " if identifier_to
+          #cmd << " HEAD " if !identifier_to
+          puts "revisions"
+          puts cmd
+          shellout(cmd) do |io|
+            files=[]
+            params={:commit=>'',:author=>'',:date=>'',:message=>'',:file=>{:path=>'',:action=>''}}
+            i=0
+            message=''
+            io.each_line do |line|
+              
+              if line=~/^commit/ and i>0
+                revisions << Revision.new({:identifier => nil,
+                                            :scmid => params[:commit],
+                                            :author => params[:author],
+                                            :time => Time.parse(params[:date]),
+                                            :message => params[:message],
+                                            :paths => files
+                                          })
+                
+                files=[]	
+                i=0
+                params={:commit=>'',:author=>'',:date=>'',:message=>'',:file=>{:path=>'',:action=>''}}
+              end
+              params[:commit]=line.chomp.gsub("commit ",'') if i==0
+              params[:author]=line.chomp.gsub("Author: ",'') if i==1
+              params[:date]=line.chomp.gsub("Date: ",'') if i==2
+              params[:message]+= line.chomp.to_s if i>=4 and line[0..0]!=':'
+              params[:file][:action], params[:file][:path]= line.chomp.slice(/[ACDMRTUXB].*/).split(' ', 2) if i>=4 and line[0..0]==':'
+              files << {:action=>params[:file][:action],:path=>params[:file][:path]}  if i>=4 and line[0..0]==':'
+              i+=1
+            end	
+          end
 
           return nil if $? && $?.exitstatus != 0
-           puts "RETURNING REVISIONS"
-           revisions
+          puts "RETURNING REVISIONS"
+          revisions
         rescue Errno::ENOENT => e
           raise CommandFailed
-	   end
+        end
         
         def diff(path, identifier_from, identifier_to=nil, type="inline")
           path ||= ''
@@ -180,7 +180,7 @@ end
           end
           return nil if $? && $?.exitstatus != 0
           DiffTableList.new diff, type
-    
+          
         rescue Errno::ENOENT => e
           raise CommandFailed
         end
